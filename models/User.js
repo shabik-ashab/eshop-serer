f8b7b6ecc9ffdd92ba93f6da82f3cbdc1755fd41
nub69 <shabik.ashab@gmail.com>
2022-05-25 23:51:22 +0600

    cart model added

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema({
     email: {  type: String, required:true, unique:true },
     password: { type: String, required:true },
     isAdmin: {
-      type: boolean,
+      type: Boolean,
       default: false,
     },
 },
